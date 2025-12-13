b6632fe0f85bee61e734d46d0c0570986f949d87
Ridham-Bhuva <ridhambhuva007@gmail.com>
2025-12-13 21:50:21 +0100

    Update porousCapillaryFoam.C

--- porousCapillaryFoam.C
+++ porousCapillaryFoam.C
@@ -190,6 +190,12 @@ int main(int argc, char *argv[])
             {
                 fluid.correctTurbulence();
             }
+
+            // Both Phases may have small residual velocities in regions where it has vanished (alpha ~ 0). 
+            // Multiplying by pos(alpha2 - alphaMin) clamps these non-physical velocities to zero,
+            // improving numerical stability, speed and preventing ghost flow in the non-wetting phase.
+            U1 *= pos(alpha1 - 1e-6);
+            U2 *= pos(alpha2 - 1e-6);
         }
 
         #include "write.H"
@@ -209,3 +215,4 @@ int main(int argc, char *argv[])
 
 
 // ************************************************************************* //
+
